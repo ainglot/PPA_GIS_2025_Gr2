c1f8abe8167c09c9e5e053e121708f3e490845d7
ainglot <ainglot@gmail.com>
2025-11-18 10:17:34 +0100

    Odczytywanie współrzędnych punktów i zapisywanie do listy

--- warstwa_punktowa.py
+++ warstwa_punktowa.py
@@ -3,8 +3,14 @@ import arcpy
 arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\PPA_Gr2.gdb"
 warstwa_punktowa = "GDA2020_OT_OIPR_P"
 
-with arcpy.da.SearchCursor(warstwa_punktowa, ["SHAPE@X", "SHAPE@Y"]) as cursor:
-    for row in cursor:
-        print(f'{row[0]}, {row[1]}')
+def odczytywanie_wspolrzednych_do_listy(warstwa):
+    lista_wsp = []
+    with arcpy.da.SearchCursor(warstwa, ["SHAPE@X", "SHAPE@Y"]) as cursor:
+        for row in cursor:
+            # print(f'{row[0]}, {row[1]}')
+            lista_wsp.append([row])
+    return lista_wsp
+
+print(odczytywanie_wspolrzednych_do_listy(warstwa_punktowa)[:50])
 
 print("KONIEC")
