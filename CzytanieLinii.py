a363b12a3c12140cf114b6498475c6dddc2e7601
ainglot <ainglot@gmail.com>
2025-11-25 10:33:13 +0100

    Odczytywanie współrzędnych wierzchołków linii

--- CzytanieLinii.py
+++ CzytanieLinii.py
@@ -10,26 +10,32 @@ warstwa_liniowa = "GDA2020_OT_SWRS_L"
 # =============================================================================
 # DEFINIOWAINE FUNKCJI DLA WARSTWY PUNKTOWEJ
 # =============================================================================
-def odczytywanie_wspolrzednych_do_listy(warstwa):
-    lista_wsp = []
-    with arcpy.da.SearchCursor(warstwa, ["SHAPE@X", "SHAPE@Y"]) as cursor:
+def odczytywanie_wspolrzednych_linii_do_listy(warstwa):
+    lista_ob = []
+    with arcpy.da.SearchCursor(warstwa, ["SHAPE@"]) as cursor:
         for row in cursor:
-            # print(f'{row[0]}, {row[1]}')
-            lista_wsp.append([row[0], row[1]])
-    return lista_wsp
-lista_wsp = odczytywanie_wspolrzednych_do_listy(warstwa_liniowa)
+            print(row)
+            list_pkt = []
+            for part in row[0]:
+                print(part)
+                for pnt in part:
+                    print(pnt.X, pnt.Y)
+                    list_pkt.append([pnt.X, pnt.Y])
+            lista_ob.append(list_pkt)
+    return lista_ob
+lista_wsp = odczytywanie_wspolrzednych_linii_do_listy(warstwa_liniowa)
 
-def wstawianie_wspolrzednych(nowa_warstwa, uklad_wsp, lista_wsp):
-    arcpy.management.CreateFeatureclass(arcpy.env.workspace, nowa_warstwa, "POINT", "", "DISABLED", "DISABLED", uklad_wsp)
-    with arcpy.da.InsertCursor(nowa_warstwa, ["SHAPE@X", "SHAPE@Y"]) as cursor:
-        for wsp in lista_wsp:
-            # cursor.insertRow([wsp[0], wsp[1]])
-            cursor.insertRow(wsp)
+# def wstawianie_wspolrzednych(nowa_warstwa, uklad_wsp, lista_wsp):
+#     arcpy.management.CreateFeatureclass(arcpy.env.workspace, nowa_warstwa, "POINT", "", "DISABLED", "DISABLED", uklad_wsp)
+#     with arcpy.da.InsertCursor(nowa_warstwa, ["SHAPE@X", "SHAPE@Y"]) as cursor:
+#         for wsp in lista_wsp:
+#             # cursor.insertRow([wsp[0], wsp[1]])
+#             cursor.insertRow(wsp)
 
-print(lista_wsp)
+print(lista_wsp[-1])
 print(len(lista_wsp))
 
-wstawianie_wspolrzednych("Centroidy_SWRS_01", warstwa_liniowa, lista_wsp)
+# wstawianie_wspolrzednych("Centroidy_SWRS_01", warstwa_liniowa, lista_wsp)
 
 
 print("KONIEC")
