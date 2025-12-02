9bfb4c69640f98c3b763a39c903d03fee2103dce
ainglot <ainglot@gmail.com>
2025-12-02 10:23:03 +0100

    Odczytywanie współrzędnych poligonu

--- warstwa_poligonowa.py
+++ warstwa_poligonowa.py
@@ -10,23 +10,31 @@ warstwa_poligonowa = "Budynek"
 # =============================================================================
 # DEFINIOWAINE FUNKCJI DLA WARSTWY PUNKTOWEJ
 # =============================================================================
+
+## [Linia1[pkt1[x1, y1], pkt2[x2, y2]....], Linia2[]...]
+## [Poligon1[graniece[pkt1[x1, y1], pkt2[x2, y2]....], dziure[pkt1[x1, y1], pkt2[x2, y2]....]], ...]
 def odczytywanie_wspolrzednych_poligonu(warstwa):
     lista_ob = []
     with arcpy.da.SearchCursor(warstwa, ["SHAPE@"]) as cursor:
         for row in cursor:
             # print(row)
-            list_pkt = []
+            list_part = []
             for part in row[0]:
                 # print(part)
+                lista_pkt = []
                 for pnt in part:
                     print(pnt)
-                    # print(pnt.X, pnt.Y)
-                    list_pkt.append([pnt.X, pnt.Y])
-            lista_ob.append(list_pkt)
+                    if pnt:
+                        lista_pkt.append([pnt.X, pnt.Y])
+                    else:
+                        list_part.append(lista_pkt)
+                        lista_pkt = []
+                list_part.append(lista_pkt)
+            lista_ob.append(list_part)
     return lista_ob
 
-odczytywanie_wspolrzednych_poligonu(warstwa_poligonowa)
+listaPOLIGONU = odczytywanie_wspolrzednych_poligonu(warstwa_poligonowa)
 
-
+print(listaPOLIGONU)
 
 print("KONIEC")
