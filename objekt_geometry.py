05f464e66bd62f3ee5590121687e228ad7474a6a
ainglot <ainglot@gmail.com>
2025-12-02 11:21:27 +0100

    Realcja stykania

--- objekt_geometry.py
+++ objekt_geometry.py
@@ -9,12 +9,31 @@ warstwa_poligonowa = "Budynki"
 
 geometries = arcpy.management.CopyFeatures(warstwa_poligonowa, arcpy.Geometry())
 
-Otoczka = []
-for geo in geometries:
-    
-    Otoczka.append(geo.convexHull())
+i = 0
+for geo1 in geometries:
+    j = 0
+    for geo2 in geometries:
+        if i < j:
+            print(i, j, geo1.touches(geo2))
+        j += 1
+    i += 1
 
-nowy_poligon = "Budynki_otoczka_01"
-arcpy.management.CopyFeatures(Otoczka, nowy_poligon)
+
+
+
+# Otoczka = []
+# for geo in geometries:
+#     AREA = geo.area 
+#     print(geo.area)
+#     if AREA < 300:
+#         geo2 = geo.buffer(1)
+#         while geo2.area < 300:
+#             geo2 = geo2.buffer(1)
+#         Otoczka.append(geo2)
+#     # else:
+#     #     Otoczka.append(geo.buffer(1).convexHull())
+
+# nowy_poligon = "Budynki_otoczka_04"
+# arcpy.management.CopyFeatures(Otoczka, nowy_poligon)
 
 print("KONIEC")
