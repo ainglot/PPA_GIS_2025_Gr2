6ece87ee20e4f0b9f622f758a9dfd35412de63d8
ainglot <ainglot@gmail.com>
2025-12-09 10:38:15 +0100

    Parametry rstra

--- raster.py
+++ raster.py
@@ -1,15 +1,28 @@
 import arcpy
 
-arcpy.env.workspace = r"C:\GIS\GeoportalDane\NMTwyk11"
-RasterIn = "4228_280105_M-34-101-A-c-3-2.asc"
+# =============================================================================
+# KONFIGURACJA DANYCH WEJŚCIOWYCH
+# =============================================================================
+arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\NMT"
+RasterIn = "81008_1561328_N-34-50-C-d-3-3.asc"
 arcpy.env.outputCoordinateSystem = arcpy.SpatialReference(2180) #przypisanie układu współrzędnych do rastra wyjściowego
+
+# =============================================================================
+# WCZYTYWANIE RASTRA PRZEZ ARCPY DO OBIEKTU "RASTER"
+# =============================================================================
 R = arcpy.Raster(RasterIn)
+print(f"Min rastra: {R.minimum}, Max: {R.maximum}")
+print(f"Rozdzielczość przestrzenna: {R.meanCellWidth}")
+
+# =============================================================================
+# POBIERAMY PARAMETRY RASTRA
+# =============================================================================
 LewyDolnyPunkt = arcpy.Point(R.extent.XMin, R.extent.YMin) #przechowanie współrzędnych do lokalizacji rastra wyjściowego
 RozdzielczoscPrzestrzenna = R.meanCellWidth #rozdzielczość przestrzenna rastra
-NoData = 0 #wartość NoData - w tym rastrze minimalna wartość jest większa niż 0, można tak wykonać
-R_array = arcpy.RasterToNumPyArray(R, nodata_to_value = NoData)
-R_array[100:200, 100:200] = 0 # W lewym gónym rógó rastra "wycinamy" prostokąt
-outR = arcpy.NumPyArrayToRaster(R_array, LewyDolnyPunkt, RozdzielczoscPrzestrzenna, value_to_nodata = NoData)
-# zapisać nowy raster trzeba podać - dane (R_array), współrzędne lewego dolnego naroża, rozdzielczość przestrzenną i jaką wartość przyjmuje NoData
-outR.save("NowyRaster")
+# NoData = 0 #wartość NoData - w tym rastrze minimalna wartość jest większa niż 0, można tak wykonać
+# R_array = arcpy.RasterToNumPyArray(R, nodata_to_value = NoData)
+# R_array[100:200, 100:200] = 0 # W lewym gónym rógó rastra "wycinamy" prostokąt
+# outR = arcpy.NumPyArrayToRaster(R_array, LewyDolnyPunkt, RozdzielczoscPrzestrzenna, value_to_nodata = NoData)
+# # zapisać nowy raster trzeba podać - dane (R_array), współrzędne lewego dolnego naroża, rozdzielczość przestrzenną i jaką wartość przyjmuje NoData
+# outR.save("NowyRaster")
 print("KONIEC")
