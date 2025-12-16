155bf0f2bc7ce38ff1812ca81e471b01de441105
ainglot <ainglot@gmail.com>
2025-12-16 10:42:11 +0100

    Punkt na rastrze.

--- linie3d.py
+++ linie3d.py
@@ -22,14 +22,29 @@ def odczytywanie_wspolrzednych_linii_do_listy(warstwa):
             lista_ob.append(list_pkt)
     return lista_ob
 
+def punkt_na_rastrze(punkt, zakres_rastra):
+    x, y = punkt
+    xmin, ymin, xmax, ymax = zakres_rastra
+
+    return xmin <= x <= xmax and ymin <= y <= ymax
+
 ListaWspLini = odczytywanie_wspolrzednych_linii_do_listy(warstwa_linie_ZTM)
 
 print(ListaWspLini)
 arcpy.env.workspace = r"D:\GIS\Rok_2025_26\PPA_ArcGIS\NMT pod ZTM\ZTM197_NMT_TIF"
 rasters = arcpy.ListRasters("*", "TIF")
+
+ListaRastrow = []
 for RasterIn in rasters:
     print(RasterIn)
     R = arcpy.Raster(RasterIn)
     print(R.extent.XMin, R.extent.YMin, R.extent.XMax, R.extent.YMax)
+    ListaRastrow.append([RasterIn, [R.extent.XMin, R.extent.YMin, R.extent.XMax, R.extent.YMax]])
+
+print(ListaRastrow)
+
+PKT = [474113.5, 718874.19] # 63,119999
+for ras in ListaRastrow:
+    punkt_na_rastrze(punkt, zakres_rastra)
 
 print("KONIEC")
